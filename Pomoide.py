6e6b970004202bf5d6255aaad0bb885969c8e31f
iniveyro <iiniveyro@gmail.com>
2022-02-04 11:09:09 -0300

    First commit of the repository

---
+++ Pomoide.py
@@ -0,0 +1,46 @@
+import time,os
+
+def countdow (count):
+	
+	while count:
+		m, s = divmod(count,60)
+		count_format = "{:02d}:{:02d}".format(m,s)
+		print(count_format)
+		time.sleep(1)
+		count = count - 1
+print(f" ")
+print(f"*----- Pomoide -----*")
+print(f" ")
+
+band="1"
+minu = int(input("Type minutes of the round: "))
+bre = int(input("Type minutes of the break: "))
+lonbre = int(input("Type minutes of the long break: "))
+rounds = int(input("Type quantity of rounds: "))
+
+print("When you be ready, press ENTER...")
+input()
+
+if band == "1":
+
+	numsess=1
+	while numsess <= rounds:
+	
+		print(f"Round: {numsess}/{rounds}")
+		print("Come on! Its time of work m8!")
+		countdow(minu*60)
+		numsess=numsess+1
+		print("Its time of a little break")
+		countdow(bre*60)
+	
+	print("Its time of a long break")
+	countdow((lonbre-bre)*60)
+	print("")
+	print("When you be restart the pomodore, press ENTER or type another key...")
+	band= input()
+	if (band != "1"):
+		rounds = int(input("How many more rounds?: "))
+	
+else:
+
+	print("FINISH")
